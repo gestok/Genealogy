0643a67fb6cb2c733cbe299d29becdeffa458971
George <giorgos.cho@hotmail.com>
2022-03-29 15:50:05 +0300

    GUI and Save

---
+++ geneapp.py
@@ -0,0 +1,11 @@
+#geneapp.py
+"""Main Application File"""
+
+
+# Global Variables
+global dict_saved
+global records_saved
+
+
+# Run GUI
+import gui.main
--- gui/main.py
+++ gui/main.py
@@ -1,69 +1,118 @@
 # main.py
 """ Main GUI Application for Tkinter """
+from socket import timeout
 import tkinter as tk
+from tkinter import ttk
+from datetime import datetime as dt
+from pathlib import Path
+import csv
+
 
 # Create Root Window
 root = tk.Tk()
-
-# Set Root's Window Title, Size and Position
+# Set Root's Window Properties
 root.title("GeneApp - Genealogy Tree Application")
-root.geometry("640x480+300+300")
-root.resizable(False, False)
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+# Create App Frame
+app = tk.Frame(root)
 
-# Information
-intro = tk.Label(
-    root,
+
+# Heading
+intro = ttk.Label(
+    app,
     text="GeneApp - Genealogy Tree Application",
     font=("Arial 16"),
-    fg="#151515"
+    foreground="#151515",
+)
+
+
+# Status Bar
+status_var = tk.StringVar()
+status = ttk.Label(
+    root,
+    textvariable=status_var,
+    foreground="#151515",
+    background="#ddd",
+    padding=5
 )
 
 # Insert Person
-insert_btn = tk.Button(root, text="Insert")
-insert_lbl = tk.Label(
-    root,
+insert_btn = ttk.Button(app, text="Insert")
+insert_lbl = ttk.Label(
+    app,
     text="Insert a new person in the Genealogy Tree or edit an existing one.",
     font=("Arial 12"),
-    bg="#ddd",
-    fg="#151515"
+    foreground="#151515",
+    background="#ddd"
 )
+
 
 # Delete Person
-delete_btn = tk.Button(root, text="Delete")
-delete_lbl = tk.Label(
-    root,
+delete_btn = ttk.Button(app, text="Delete")
+delete_lbl = ttk.Label(
+    app,
     text="Delete an existing person from the Genealogy Tree.",
     font=("Arial 12"),
-    bg="#ddd",
-    fg="#151515"
+    background="#ddd",
+    foreground="#151515"
 )
+
 
 # View Graph Tree
-view_btn = tk.Button(root, text="View Tree")
-view_lbl = tk.Label(
-    root,
+view_btn = ttk.Button(app, text="View Tree")
+view_lbl = ttk.Label(
+    app,
     text="View a network graph of the current Genealogy Tree.",
     font=("Arial 12"),
-    bg="#ddd",
-    fg="#151515"
+    background="#ddd",
+    foreground="#151515"
 )
 
+
 # Save
-save_btn = tk.Button(root, text="Save")
-save_output = tk.Label(root, text="")
+def on_save():
+    """To be run when the user tries to save progress"""
+    # Get Date Formatted String
+    datestring = dt.today().strftime("%Y-%m-%d")
+    # Generate Filename
+    filename = f"geneapp_{datestring}.csv"
+    # Check if filename exists in executable's path
+    newfile = not Path(filename).exists()
+    # Update Status Text
+    status_var.set("Saving...")
+    with open(filename, 'a', newline='') as fh:
+        csvwriter = csv.DictWriter(fh, fieldnames=["ID","First","Last","Birth","Description","Connections"])
+        if newfile:
+            csvwriter.writeheader()
+        csvwriter.writerow({"ID":1,"First":"Takis","Last":"Takitzis","Birth":"2/2/1990","Description":"Takis is a cool guy!","Connections":"1/3/5"})
+        done = True
+    if done:
+        status_var.set("Saved!")
+
+save_btn = ttk.Button(app, text="Save", command=on_save)
+
 
 # Load
-load_btn = tk.Button(root, text="Load")
+load_btn = ttk.Button(app, text="Load")
 
-# Place Widgets
-intro.grid(row=0, column=0, columnspan=6, sticky="we")
-insert_btn.grid(row=1, column=0, columnspan=2, sticky="we")
-insert_lbl.grid(row=1, column=2, columnspan=4, sticky="we")
-delete_btn.grid(row=2, column=0, columnspan=2, sticky="we")
-delete_lbl.grid(row=2, column=2, columnspan=4, sticky="we")
-view_btn.grid(row=3, column=0, columnspan=2, sticky="we")
-view_lbl.grid(row=3, column=2, columnspan=4, sticky="we")
-# save_btn.grid(row=4 )
+
+# Place Widgets in App Frame
+intro.grid(row=0, columnspan=10, sticky=tk.W + tk.E, pady=10)
+insert_btn.grid(row=1, columnspan=4, sticky=tk.W + tk.E, pady=4, padx=4)
+insert_lbl.grid(row=1, column=4, columnspan=6, sticky=tk.W + tk.E, pady=4, padx=4)
+delete_btn.grid(row=2, columnspan=4, sticky=tk.W + tk.E, pady=4, padx=4)
+delete_lbl.grid(row=2, column=4, columnspan=6, sticky=tk.W + tk.E, pady=4, padx=4)
+view_btn.grid(row=3, columnspan=4, sticky=tk.W + tk.E, pady=4, padx=4)
+view_lbl.grid(row=3, column=4, columnspan=6, sticky=tk.W + tk.E, pady=4, padx=4)
+save_btn.grid(row=4, column=8, sticky=tk.W + tk.E, pady=4, padx=4)
+load_btn.grid(row=4, column=9, sticky=tk.W + tk.E, pady=4, padx=4)
+
+
+# Place Frame in Root Window
+app.grid(padx=14, pady=14)
+status.grid(sticky=tk.W + tk.E)
+
 
 # Start event loop
 root.mainloop()
